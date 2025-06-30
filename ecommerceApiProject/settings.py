805f81ef260dc4e7f77f8f02b436f55ef15c9287
Royal1king <theroyalking112233@gmail.com>
2025-06-30 17:25:17 +0530

    deployment

--- ecommerceApiProject/settings.py
+++ ecommerceApiProject/settings.py
@@ -146,9 +146,9 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR/"staticfiles"
 STORAGES = {
-    # "default": {
-    #     "BACKEND": "django.core.files.storage.FileSystemStorage",
-    # },
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
